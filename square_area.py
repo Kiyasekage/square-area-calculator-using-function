27ce45fbc30fbe6635f627ed8b1e1008c5a200e9
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-07-03 15:04:11 +0800

    square_area.py

---
+++ square_area.py
@@ -0,0 +1,6 @@
+def square(num):
+    area = pow(num,2)
+    print(f"The area of square is {area}")
+print("Welcome to square area calculator")
+side = int(input("Please enter the length of the square side : "))
+square(side)
